5600b0bc72c988d9524fa488ad238489859c9752
leegyeongwhan <leekhy02@naver.com>
2022-10-05 15:48:06 +0900

    [1 단계] Stage 변경

--- src/main/java/sokoban/Data/Stage.java
+++ src/main/java/sokoban/Data/Stage.java
@@ -1,90 +1,75 @@
 package sokoban.Data;
 
-import sokoban.Status;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class Stage {
-    static ArrayList<ArrayList<String>> map;
-    static ArrayList<ArrayList<String>> numMap;
+    private char[][] chMap;
+    private int[][] intMap;
+    private int stageNumber;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        map = new ArrayList<>();
-        String input = "";
-        numMap = new ArrayList<>();
+    private Point mapSize;  //가로 세로
+    private Point playerLocation;
+    private int hallCount;
+    private int ballCount;
 
-        while (true) {
-            input = br.readLine();
-            if (input == null || input.equals("")) {
-                break;
-            }
-            map.add(new ArrayList<>(Collections.singleton(input)));
-        }
-        readerInfoMap();
-        //       System.out.println(map);
-        System.out.println(numMap);
-        printMap();
-        new Status(numMap);
+    public Stage(List<String> lines, int stageNumber) {
+        System.out.println(lines);
+        this.chMap = StageUtil.changeChMap(lines);
+        this.intMap = StageUtil.changeInnMap(lines);
+
+        System.out.println(Arrays.deepToString(chMap));
+        System.out.println(Arrays.deepToString(intMap));
+        this.stageNumber = stageNumber;
+    }
+
+
+    public void setMap(char[][] map) {
+        int height = map.length;
+        int width = map[0].length;
+
 
     }
 
-    private static void readerInfoMap() {
-        for (int i = 0; i < map.size(); i++) {
-            //System.out.println(String.valueOf(map.get(i)));
-            String[] str = String.valueOf(map.get(i)).split("");
-            numMap.add(new ArrayList<>());
-            for (int j = 0; j < str.length; j++) {
-                // System.out.println(str[j]);
-                if (str[j].equals("#")) {
-                    numMap.get(i).add("0");
-                } else if (str[j].equals("O")) {
-                    numMap.get(i).add("1");
-                } else if (str[j].equals("O")) {
-                    numMap.get(i).add("2");
-                } else if (str[j].equals("P")) {
-                    numMap.get(i).add("3");
-                } else if (str[j].equals("=")) {
-                    numMap.get(i).add("4");
-                } else {
-                    numMap.get(i).add("");
-                }
-            }
-        }
+
+    public int getStageNumber() {
+        return stageNumber;
     }
 
-    private static void printMap() {
-        for (int i = 0; i < map.size(); i++) {
-            String str = String.join(" ", map.get(i));
-            //  System.out.println("str=" + str);
-            if (str.contains("Stage")) {
-                System.out.println(str);
-                System.out.println();
-                continue;
-            } else if (str.contains("=")) {
-                printInfo();
-                continue;
-            } else {
-                System.out.print(str);
-                System.out.println();
-            }
-            if (i == map.size() - 1) {
-                printInfo();
-            }
-        }
+    public void setStageNumber(int stageNumber) {
+        this.stageNumber = stageNumber;
     }
 
-    private static void printInfo() {
-        System.out.println();
-        System.out.println("가로크기: " + Status.width);
-        System.out.println("세로크기: " + Status.height);
-        System.out.println("구멍의 수: " + Status.hallcount);
-        System.out.println("공의 수: " + Status.ballcount);
-        System.out.println("플레이어 위치");
-        System.out.println();
+    public Point getMapSize() {
+        return mapSize;
+    }
+
+    public void setMapSize(Point mapSize) {
+        this.mapSize = mapSize;
+    }
+
+    public Point getPlayerLocation() {
+        return playerLocation;
+    }
+
+    public void setPlayerLocation(Point playerLocation) {
+        this.playerLocation = playerLocation;
+    }
+
+    public int getHallCount() {
+        return hallCount;
+    }
+
+    public void setHallCount(int hallCount) {
+        this.hallCount = hallCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public void setBallCount(int ballCount) {
+        this.ballCount = ballCount;
     }
 }
