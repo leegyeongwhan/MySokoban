7e06cc559d33b3bdd680f4fff1477a2d58183ae1
leegyeongwhan <leekhy02@naver.com>
2022-10-05 14:26:39 +0900

    [1 단계] ArrayList 로 받은 line을 char 타입으로 바꿔주는 메서드 추가

--- src/main/java/sokoban/Data/StageUtil.java
+++ src/main/java/sokoban/Data/StageUtil.java
@@ -1,5 +1,7 @@
 package sokoban.Data;
 
+import java.util.List;
+
 public class StageUtil {
 
     public static boolean isStartStage(String line) {
@@ -14,5 +16,14 @@ public class StageUtil {
             return true;
         }
         return false;
+    }
+
+    public static char[][] changeChMap(List<String> lines) {
+        char[][] map = new char[lines.size()][];
+
+        for (int i = 0; i < lines.size(); i++) {
+            map[i] = lines.get(i).toCharArray();
+        }
+        return map;
     }
 }
