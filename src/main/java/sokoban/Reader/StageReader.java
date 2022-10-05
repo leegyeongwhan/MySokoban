9dd3f5d6cecc31442f2dd0664b1195f26d40ccfd
leegyeongwhan <leekhy02@naver.com>
2022-10-05 13:40:21 +0900

    [1 단계] StageReader readAllMap 맵의 전체 데이터 읽기 추가

--- src/main/java/sokoban/Reader/StageReader.java
+++ src/main/java/sokoban/Reader/StageReader.java
@@ -2,7 +2,10 @@ package sokoban.Reader;
 
 import sokoban.Data.Stage;
 
-public interface StageReader {
-    Stage readGameMap();
+import java.io.IOException;
+import java.util.List;
 
+public interface StageReader {
+    Stage readGameMap() throws IOException;
+    List<Stage> readAllMap() throws IOException;
 }
