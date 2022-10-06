a98fa4efe1b0a3919ea143417283ba02bf78d680
leegyeongwhan <leekhy02@naver.com>
2022-10-06 13:23:21 +0900

    [1 단계]  prointMap메서드 구현

--- src/main/java/sokoban/Data/Stage.java
+++ src/main/java/sokoban/Data/Stage.java
@@ -29,23 +29,35 @@ public class Stage {
     public void setMap(char[][] charMap, int[][] intMap) {
         int height = charMap.length;
         int width;
+        int widthMax = 0;
 
         for (int i = 0; i < height; i++) {
             width = charMap[i].length;
+            widthMax = Math.max(widthMax, width);
             for (int j = 0; j < width; j++) {
-                System.out.print(charMap[i][j]);
 
                 int value = intMap[i][j];
                 if (Sign.BALL.getValue() == value) ballCount++;
                 if (Sign.HALL.getValue() == value) hallCount++;
                 if (Sign.PLAYER.getValue() == value) playerLocation = new Point(i, j);
             }
-            System.out.println();
         }
+        mapSize = new Point(widthMax, height);
     }
 
-    public char[][] getChMap() {
-        return chMap;
+    public void printMap() {
+        int height = chMap.length;
+        int width;
+
+        System.out.println();
+        for (int i = 0; i < height; i++) {
+            width = chMap[i].length;
+            for (int j = 0; j < width; j++) {
+                System.out.print(chMap[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public int getStageNumber() {
