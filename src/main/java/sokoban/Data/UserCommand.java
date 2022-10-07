a8e5a925e071be508a26af114877206efdf3f25b
leegyeongwhan <leekhy02@naver.com>
2022-10-07 14:19:21 +0900

    [2 단계]  UserCommand 명령어추가

--- src/main/java/sokoban/Data/UserCommand.java
+++ src/main/java/sokoban/Data/UserCommand.java
@@ -1,22 +1,27 @@
 package sokoban.Data;
 
-import java.util.Arrays;
-
 public enum UserCommand {
-    W(new Point(-1, 0)), // 위
-    A(new Point(0, -1)), // 왼쪽
-    S(new Point(1, 0)),  // 아래
-    D(new Point(0, 1)),  // 오른쪽
-    Q(new Point(0, 0));  // 프로그램 종료
+    W(new Point(-1, 0), "위로 이동합니다."), // 위
+    A(new Point(0, -1), "왼쪽으로 이동합니다."), // 왼쪽
+    S(new Point(1, 0), "아래쪽으로 이동합니다."),  // 아래
+    D(new Point(0, 1), "오른쪽으로 이동합니다."),  // 오른쪽
+    Q(new Point(0, 0), "프로그램을 종료합니다");  // 프로그램 종료
+
+    UserCommand(Point point, String message) {
+        this.direction = point;
+        this.message = message;
+    }
 
     //플레이어 이동
     private Point direction;
 
-    UserCommand(Point point) {
-        this.direction = point;
-    }
+    public int value;
+    String message;
+
 
     public Point getDirection() {
         return direction;
     }
+
+
 }
