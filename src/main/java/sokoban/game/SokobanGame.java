45988ae46ad01641c4e47d98c9cc834d240431e2
leegyeongwhan <leekhy02@naver.com>
2022-10-07 12:40:16 +0900

    [2 단계]  sokbanGaem 클래스 작성 게임 플레이를 할 때 클리어했는지,플레이할 스테이지 선택 등의 로직을 담당

--- src/main/java/sokoban/game/SokobanGame.java
+++ src/main/java/sokoban/game/SokobanGame.java
@@ -1,4 +1,33 @@
 package sokoban.game;
 
+import sokoban.Reader.CmdStageReaderImpl;
+import sokoban.Reader.StageReader;
+import sokoban.Wirtter.CmdStageWriterImpl;
+import sokoban.Wirtter.StageWriter;
+import sokoban.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SokobanGame {
+
+    private List<Stage> stageList = new ArrayList<>();
+
+    public SokobanGame(List<Stage> stageList) throws IOException {
+        if (stageList.size() < 1) {
+            throw new IllegalArgumentException("올바르지 않은 스테이지 목록입니다");
+        }
+        //스테이지 호출
+        System.out.println("플레이할 스테이지 입력");
+        StageReader reader = new CmdStageReaderImpl();
+        StageWriter writer = new CmdStageWriterImpl();
+        Stage stage = reader.readStageMap();
+        writer.writeStageMap(stage);
+    }
+
+    private void firstStageGame(Stage stage) {
+        PlayGame playGame = new PlayGame();
+        playGame.gameSet(stage);
+    }
 }
