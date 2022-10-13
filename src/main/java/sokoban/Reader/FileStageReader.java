d7f1490e0de924759d0f2823a36334ce234c522f
leegyeongwhan <leekhy02@naver.com>
2022-10-14 00:26:58 +0900

    [3 단계]   moveProcess 수정

---
+++ src/main/java/sokoban/Reader/FileStageReader.java
@@ -0,0 +1,18 @@
+package sokoban.Reader;
+
+import sokoban.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
+public class FileStageReader implements StageReader {
+    @Override
+    public List<Stage> readAllGameMap() throws IOException {
+        return null;
+    }
+
+    @Override
+    public Stage readStageMap() throws IOException {
+        return null;
+    }
+}
